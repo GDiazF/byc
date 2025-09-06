ac0e2dafe5cf95562f9345cc6630e6e0cc2e58f8
GDiazF <memodiazflores@gmail.com>
2025-09-06 16:26:07 -0400

    s

--- bycCore/settings.py
+++ bycCore/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-zxiwr5sw3xn%vu+bh47ucrprmj2c@ws#0%+x22if%gcqj16pbx'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["54.91.203.50"]
 
 
 # Application definition
